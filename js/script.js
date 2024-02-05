903458fd086bad3f23c801cae0c5915e08a764e9
Alexa Ped <alexaped16@gmail.com>
2024-02-05 13:21:40 -0600

    smaller images

--- js/script.js
+++ js/script.js
@@ -1,4 +1,54 @@
-/* toggle hamburger */
+function sendEmail() {
+    const name = document.getElementById('txtName').value;
+    const email = document.getElementById('txtEmail').value;
+    const phone = document.getElementById('txtPhone').value;
+    const company = document.getElementById('txtCompany').value;
+    const product = document.getElementById('txtProduct').value;
+    const message = document.getElementById('txtMessage').value;
+  
+    const emailBody = `
+      <h2>Contact Form submitted from AKT</h2>
+      <div>Name: <strong>${name}</strong></div>
+      <div>Email: <strong>${email}</strong></div>
+      <div>Phone: <strong>${phone}</strong></div>
+      <div>Company: <strong>${company}</strong></div>
+      <div>Product: <strong>${product}</strong></div>
+      <div>Message: <strong>${message}</strong></div>
+    `;
+  
+    const payload = {
+      Body: emailBody,
+      ToEmails: 'sales@aktinc.com',
+      Subject: 'AKT Contact Form',
+      FromName: 'AKT',
+      FromEmail: 'alexa.pedersen@alexapedersen.com'
+    };
+  
+    fetch('https://gn02-email-test.azurewebsites.net/api/SendEmail', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+      .then(response => {
+        if (response.ok) {
+          // Display success message here
+          document.getElementById('formMessage').textContent = 'Your form has been successfully sent!';
+          document.getElementById('formMessage').classList.add('success');
+          console.log('Email sent successfully!');
+        } else {
+          console.error(response);
+          throw new Error('Failed to send email.');
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error.message);
+      });
+  }
+  
+  
+  /* toggle hamburger */
 
 const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".nav-menu");
